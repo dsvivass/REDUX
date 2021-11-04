218dc3e325f80fca2680e80064dd66ae126f326b
dsvivass <dsvivass@unal.edu.co>
2021-11-03 22:16:30 -0500

    Final crash-course

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,7 +1,23 @@
+
+import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+
+
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      
+      <button onClick={() => dispatch({type: 'INCREMENT', payload: 5})}>+</button>
+      <button onClick={() => dispatch({type: 'DECREMENT', payload: 3})}>-</button>
+
+      {isLogged ? 
+      <h3>Valuable info I shouldn't see</h3> : <h3>Login</h3>}
+      
     </div>
   );
 }
--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,51 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore} from 'redux'
+import {createStore} from 'redux';
+import allReducers from './reducers' // No tengo que poner el index porque lo reconoce automaticamente
+import {Provider} from 'react-redux';
 
-// STORE --> GLOBALIZED STATE
-// var store = createStore()
-
-// ACTION
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-// REDUCER
-
-const counter = (state = 0, action) => {
-  switch (action.type){
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-  }
-}
-
-var store = createStore(counter, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()  
-  )
-
-// Display it in console
-
-store.subscribe(() => console.log(store.getState()))
-
-// DISPATCH
-store.dispatch({type: 'INCREMENT'})
-store.dispatch({type: 'DECREMENT'})
-store.dispatch({type: 'DECREMENT'})
+const store = createStore(allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
